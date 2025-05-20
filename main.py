4b17387fa359b9945367d553f846fa233d6e3aa9
Tristan Duarte <tduar10@wgu.edu>
2025-05-20 09:42:36 -0700

    Update main.py
    add portion to read distances csv file

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+# Load distance data
+distance_df = pd.read_csv('distances.csv', header=None)
+name_df = pd.read_csv('dest_names.csv')
+
+# Fill the upper triangle so that i, j == j, i
+for i in range(len(distance_df)):
+    for j in range(len(distance_df.columns)):
+        if pd.isna(distance_df.iloc[i, j]):
+            distance_df.iloc[i, j] = distance_df.iloc[j, i]
+
+# Store addresses
+addresses = name_df['Addr'].tolist()
+
+# Convert DataFrame to 2D list
+distance_matrix = distance_df.astype(float).values.tolist()
+
+package_df = pd.read_csv('WGUPS Package File.csv')
